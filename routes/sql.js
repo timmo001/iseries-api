c1e8108f8158d8cb5b68107980e12825e6e0d7a1
Timmo <contact@timmo.xyz>
2018-10-05 09:56:47 +0100

    :sparkles: support for selects

--- routes/sql.js
+++ routes/sql.js
@@ -1,12 +1,28 @@
 module.exports = (app, jsonParser) => {
   app.post('/sql', jsonParser, (req, res) => {
-    if (req.body.username) {
-      if (!user.password) {
-        console.log('Bad password'); res.status(400).send('No Password! You must provide one!');
-        // console.log('Bad password'); res.status(400).send('Incorrect password. Check your spelling and try again.');
-      } else {
-        res.status(200).json(setConfig({ ok: true }));
+    if (!req.body.username) { console.log('Bad username'); res.status(400).send('No Username! You must provide one!'); }
+    else if (!req.body.password) { console.log('Bad password'); res.status(400).send('No Password! You must provide one!'); }
+    else if (!req.body.hostname) { console.log('Bad hostname'); res.status(400).send('No hostname! You must provide one!'); }
+    else if (!req.body.command) { console.log('Bad command'); res.status(400).send('No command! You must provide one!'); }
+    else {
+      const config = {
+        host: req.body.hostname,
+        user: req.body.username,
+        password: req.body.password
       }
-    } else console.log('Bad username'); res.status(400).send('No Username! You must provide one!');
+      const pool = require('node-jt400').pool(config);
+
+      if (req.body.command.toUpperCase().startsWith('SELECT')) {
+        pool.query(req.body.command)
+          .then(result => {
+            console.log('result:', result);
+            res.status(200).json(result);
+          })
+          .fail(error => {
+            console.log('error:', error);
+            res.status(400).send(error);
+          });
+      }
+    }
   });
 };
