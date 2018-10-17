5a06a95bfea381137e4eb2dd9ea14308ab0849e4
Timmo <contact@timmo.xyz>
2018-10-17 12:39:18 +0100

    :hammer: only get columns once with ws

--- websocket/run.js
+++ websocket/run.js
@@ -5,6 +5,7 @@ class Run {
   constructor(ws, message, func) {
     func(message, res => respond(ws, message.request, res));
     interval = setInterval(() => {
+      if (message.get_columns) message.get_columns = false;
       func(message, res => respond(ws, message.request, res));
     }, message.update_ms || 10000);
   }
